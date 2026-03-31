a767e6f449b03b4fb76934b13a130624ea71c6d4
linska <linskatanya@gmail.com>
2026-03-31 09:48:49 +0300

    feat: implement calculateRentalCost function

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -1,10 +1,27 @@
+'use strict';
+
 /**
  * @param {number} days
  *
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const dailyCost = 40;
+  const longTerm = 7;
+  const shortTerm = 3;
+  const longTermDiscount = 50;
+  const shortTermDiscount = 20;
+  const basePrice = days * dailyCost;
+
+  if (days >= longTerm) {
+    return basePrice - longTermDiscount;
+  }
+
+  if (days >= shortTerm) {
+    return basePrice - shortTermDiscount;
+  }
+
+  return basePrice;
 }
 
 module.exports = calculateRentalCost;
